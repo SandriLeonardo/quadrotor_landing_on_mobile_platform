77029a12284607591065a4a05b3a31ebd02e8eea
2137374 <leosandri99@gmail.com>
2025-04-23 20:03:11 +0200

    visualization script bug fixing

--- visualize_in_coppelia.m
+++ visualize_in_coppelia.m
@@ -1,6 +1,3 @@
-% Load simulation results
-% (Save position and orientation data)
-
 % Start Coppelia connection
 sim = remApi('remoteApi');
 sim.simxFinish(-1);
@@ -13,19 +10,29 @@ if (clientID > -1)
     % Start simulation in CoppeliaSim
     sim.simxStartSimulation(clientID, sim.simx_opmode_blocking);
 
-        % Replay trajectory
-    for i = 1:length(out.quadrotor_states.time)
-        position = out.quadrotor_states.signals.values(i, 1:3);
-        orientation = out.quadrotor_states.signals.values(i, 4:6);
-        
-        % Send to CoppeliaSim
-        sim.simxSetObjectPosition(clientID, quadHandle, -1, position, sim.simx_opmode_oneshot);
-        sim.simxSetObjectOrientation(clientID, quadHandle, -1, orientation, sim.simx_opmode_oneshot);
-        
-        % Add delay to match simulation rate
-        pause(0.01); % Adjust based on simulation step
+    % Get data dimensions and reshape if needed
+    data = out.quadrotor_states.signals.values;
+    time = out.quadrotor_states.time;
+
+    % Subsample for visualization (every 100th point)
+    step = 50;
+
+    % Replay trajectory
+    for i = 1:step:length(time)
+        % Check bounds
+        if i <= size(data, 1)
+            position = data(i, 1:3);
+            orientation = data(i, 4:6); 
+            % Send to CoppeliaSim
+            sim.simxSetObjectPosition(clientID, quadHandle, -1, position, sim.simx_opmode_oneshot);
+            sim.simxSetObjectOrientation(clientID, quadHandle, -1, orientation, sim.simx_opmode_oneshot);
+            
+            % Add delay to make visualization visible
+            pause(1);
+        end
     end
 
     % Stop simulation
     sim.simxStopSimulation(clientID, sim.simx_opmode_blocking);
+    sim.delete();
 end
